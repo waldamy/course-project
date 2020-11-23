45e662d8e045b50e50bf9b696b6d29df59f867a8
Waldamy <volodymyr.dukhonchenko@gmail.com>
2020-11-23 17:34:52 +0200

    Initial commit

---
+++ app/src/main/java/com/penguinproject/courseproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.penguinproject.courseproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
